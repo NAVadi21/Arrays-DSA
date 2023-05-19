13ad086cce759eb5e60f167dbf81959f810ec384
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-05-19 13:25:56 +0530

    Update TwoDArray2Insert.java Implementation

--- TwoDArray2Insert.java
+++ TwoDArray2Insert.java
@@ -1,41 +1,39 @@
 package practice;
-
 import java.util.Scanner;
 
-public class TwoDArray2Insert {
-	
-	//function to insert the elements
-	static void InsertElement(int arr[][], Scanner sc, int row, int col) {
-		System.out.println("Enter array elements");
-		//insert loop
-		for(int i=0; i<row; i++) {
-			for(int j=0; j<col; j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		
-		//print loop
-		for(int i=0; i<row; i++) {
-			for(int j=0; j<col; j++) {
-				System.out.print(arr[i][j]+" ");
-			}
-		}
-	}
+public class TwoDArray {
 
-	//driver code
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter row ");
-		int row = sc.nextInt();
-		System.out.println("Enter col");
-		int col = sc.nextInt();
-		
-		int arr[][] = new int[row][col];
-		
-		//call the function to insert the array elements
-		
-		TwoDArray2Insert.InsertElement(arr, sc, row, col);
-	}
+    public static void main(String[] args) {
 
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the number of rows
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        // Prompt the user to enter the number of columns
+        System.out.println("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+
+        // Create a 2D array to store the user input
+        int[][] arr = new int[rows][columns];
+
+        // Prompt the user to enter the elements of the array
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.println("Enter the element at row " + i + " and column " + j + ": ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Print the 2D array
+        System.out.println("The 2D array: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
